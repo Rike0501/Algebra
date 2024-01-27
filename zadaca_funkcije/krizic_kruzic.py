de3652f6e797e7b1b6aacb61e295c072c2f96d08
Erik Bastijanic <erikbastijanic@yahoo.com>
2024-01-27 21:42:17 +0100

    Kreirao funkcije za krizic i kruzic

--- zadaca_funkcije/krizic_kruzic.py
+++ zadaca_funkcije/krizic_kruzic.py
@@ -4,6 +4,32 @@ red_b = [' ', ' ', ' ']
 red_c = [' ', ' ', ' ']
 
 
+def krizic(unos): 
+    lista = list(unos)
+    if lista[0] == 'a':
+        red_a.insert(int(lista[1]), 'X')
+        return red_a[int(lista[1])]
+    elif lista[0] == 'b':
+        red_b.insert(int(lista[1]), 'X')
+        return red_b[int(lista[1])]
+    elif lista[0] == 'c':
+        red_b.insert(int(lista[1]), 'X')
+        return red_c[int(lista[1])]
+    
+
+
+def kruzic(unos): 
+    lista = list(unos)
+    if lista[0] == 'a':
+        red_a.insert(int(lista[1]), 'O')
+        return red_a[int(lista[1])]
+    elif lista[0] == 'b':
+        red_b.insert(int(lista[1]), 'O')
+        return red_b[int(lista[1])]
+    elif lista[0] == 'c':
+        red_b.insert(int(lista[1]), 'O')
+        return red_c[int(lista[1])]
+
 
 
 print()
